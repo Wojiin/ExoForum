7e3586169a96790b23d83fe804387f921d3fc264
Quentin Maia <q.maia@laposte.net>
2025-11-07 17:00:54 +0100

    Add files via upload

--- view/forum/listCategories.php
+++ view/forum/listCategories.php
@@ -1,12 +1,12 @@
 <?php
-    $categories = $result["data"]['categories']; 
+    $categories = $result["data"]['category']; 
 ?>
 
 <h1>Liste des catégories</h1>
 
 <?php
 foreach($categories as $category ){ ?>
-    <p><a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $category->getId() ?>"><?= $category->getName() ?></a></p>
+    <p><a href="index.php?ctrl=forum&action=listTopicsByCategory&id=<?= $category->getIdCategory() ?>"><?= $category->getName() ?></a></p>
 <?php }
 
 
